7e28f21fff900f9b7c04c446a606202f987a6e3c
GautamHingrajiya <137255545+GautamHingrajiya@users.noreply.github.com>
2023-09-10 01:05:30 +0530

    push

--- myenv/digitalsociety/digitalsociety/settings.py
+++ myenv/digitalsociety/digitalsociety/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ozd9cee%jp_8#1xd9078z^8-vx&ev!(g@3t(4c0gnih7xtm-18'
+SECRET_KEY = 'django-insecure-**&22*j^g69ap)rgvfp$3@wucj=1(yy0ds2(2!wrcqp5ypsr=#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- myenv/digitalsociety/myapp/views.py
+++ myenv/digitalsociety/myapp/views.py
@@ -1,34 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import *
+
+
 # Create your views here.
-def home(request):
-    return render(request,"myapp/index.html")
 
 def login(request):
-    
-    if request.POST:
-        p_email = request.POST['email']
-        p_password = request.POST['password']
-
-        uid = User.objects.get(email = p_email)
-        print("\n\t\n\t UID Object :",uid )
-
-        if uid.role=="Chairman":
-            cid = Chairman.objects.get(user_id = uid)
-            print(cid)
-        else:
-            pass
-        
-        
-
-        # if uid.password == p_password:
-        #     print("valid Password")
-         
-        # else:
-        #     print("Invalid Password")
-
-    else :
-        print("\n\n\t\tonly refresh")
-
     return render(request,"myapp/login.html")
