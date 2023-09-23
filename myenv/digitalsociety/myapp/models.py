e3673241437b30266fe823dd2857e423aa0d743d
GautamHingrajiya <gautamhingrajiya@gmail.com>
2023-09-23 12:51:25 +0530

    push

--- myenv/digitalsociety/myapp/models.py
+++ myenv/digitalsociety/myapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(unique=True,max_length=50)
+    password = models.CharField(max_length=30)
+    role = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.email
+
+class Chairman(models.Model):
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    firstname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+    contactno = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.firstname
+
+class SocietyMember(models.Model):
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    firstname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+    contactno = models.CharField(max_length=30)
+    no_of_familymember = models.CharField(max_length=30)
+    house_no = models.CharField(max_length=10)
+    vehicle_details = models.CharField(max_length=30)
+    no_of_vehicle = models.CharField(max_length=10)
+    occupation = models.CharField(max_length=30)
+    job_address = models.CharField(max_length=100,null=True,blank=True)
--- myenv/digitalsociety/myapp/views.py
+++ myenv/digitalsociety/myapp/views.py
@@ -1,8 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import *
 
 # Create your views here.
 
+def home(request):
+    if "email" in request.session:
+        return render(request,"myapp/index.html")
+    else:
+        return render(request,"myapp/login.html")
+
 def login(request):
+    
+    if request.POST:
+
+        p_email = request.POST['email']
+        p_password = request.POST['password']
+
+        try:
+            uid = User.objects.get(email = p_email)
+            if uid.password == p_password:
+                if uid.role == "Chairman":
+                    cid = Chairman.objects.get(user_id = uid)
+                    request.session['email'] = uid.email
+                    return render(request,"myapp/index.html")
+                else:
+                    pass
+            else:
+                context = {
+                    'e_msg':'Invalid Password'
+                }
+                return render(request,"myapp/login.html",context)
+        except:
+                context = {
+                    'e_msg':'Invalid Email'
+                }
+                return render(request,"myapp/login.html",context)
+            
+    else:
+
+        print("<<<<<<<<<<================= Only Referes ================== ")
+    
     return render(request,"myapp/login.html")
